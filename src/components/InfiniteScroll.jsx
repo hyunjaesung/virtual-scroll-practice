f167dab438f0c468c5d7910e125b54adf0328240
hyunjae <stevysung@gmail.com>
2021-09-23 12:56:45 +0900

    init

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import axios from '../utils/fakeAxios';
 
 const InfiniteScroll = props => {
+    const ref = useRef(null);
+    const [data, setData] = useState([]);
+
+    const callApi = async () => {
+        const listData = await axios.get();
+        setData(listData);
+    }
+
+    useEffect(() => {
+        callApi(); 
+    }, [])
+
     return (
-        <div>
-            
+        <div className="infiniteScroll">
+            {data.map(({id, name}) => (
+                <div className="infiniteScroll-item" key={id}>
+                    <span>{name}</span>
+                </div>
+            ))}
+            <div ref={ref}></div>
         </div>
     )
 }
 
 export default InfiniteScroll
+
