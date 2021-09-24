741548d934312d5dae1754535dec6d558ced8248
hyunjae <stevysung@gmail.com>
2021-09-24 10:23:58 +0900

    기본 스크롤 구현

--- src/components/InfiniteScroll.jsx
+++ src/components/InfiniteScroll.jsx
@@ -1,27 +1,52 @@
 import React, { useEffect, useRef, useState } from 'react'
 import axios from '../utils/fakeAxios';
 
+const VISIBLE_COUNT = 10;
+const ITEM_HEIGHT = 100;
+
 const InfiniteScroll = props => {
-    const ref = useRef(null);
     const [data, setData] = useState([]);
 
     const callApi = async () => {
         const listData = await axios.get();
         setData(listData);
+        setViewData(listData.slice(0, VISIBLE_COUNT));
     }
 
     useEffect(() => {
-        callApi(); 
+        callApi();
     }, [])
 
+    const [viewData, setViewData] = useState([]);
+    const [paddingTop, setPaddingTop] = useState(0);
+
+    const viewportHeight = VISIBLE_COUNT * ITEM_HEIGHT;
+    const totalHeight = data.length * ITEM_HEIGHT
+
+
+    const handleScroll = ({ target: { scrollTop } }) => {
+        console.log('scroll', scrollTop);
+
+        const startIdx = Math.floor(scrollTop / ITEM_HEIGHT);
+        const endIdx = Math.min(startIdx + VISIBLE_COUNT, data.length - 1)
+        const newViewData = data.slice(startIdx, endIdx);
+        const newPaddingTop = Math.max(startIdx * ITEM_HEIGHT, 0);
+        console.log(startIdx, endIdx, newPaddingTop);
+        setPaddingTop(newPaddingTop)
+        setViewData(newViewData);
+    }
+    console.log('data',data, viewData);
     return (
-        <div className="infiniteScroll">
-            {data.map(({id, name}) => (
-                <div className="infiniteScroll-item" key={id}>
-                    <span>{name}</span>
+        <div className="infiniteScroll" style={{ height: totalHeight }}>
+            <div className="viewPort" onScroll={handleScroll} style={{ height: viewportHeight - 100, overflowY: 'auto' }}>
+                <div className="buffer" style={{ paddingTop }}>
+                {viewData.map(({id, name}) => (
+                    <div className="infiniteScroll-item" key={id} style={{ height: ITEM_HEIGHT }}>
+                        <span>{name}</span>
+                    </div>
+                ))}
                 </div>
-            ))}
-            <div ref={ref}></div>
+            </div>
         </div>
     )
 }
